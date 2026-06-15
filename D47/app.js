11656e8a599dbfe36923b6e624291c18e4c317de
deniz_huseynli <denizhuseynli2011@gmail.com>
2026-06-15 18:03:13 +0300

    JS Task

---
+++ D47/app.js
@@ -0,0 +1,23 @@
+var ad = "Dəniz";
+let soyad = "Hüseynli";
+const yas = 15;
+
+alert(ad);
+alert(soyad);
+alert(yas);
+
+const kitab = "Percy Jackson";
+// kitab = "Harry Potter" 
+
+let meyve = "banan";
+document.write(meyve);
+
+var terevez = "pomidor";
+var terevez = "xiyar";
+console.log(terevez);
+
+let tetbiq = "Instagram";
+// let tetbiq = "YouTube";
+
+let adim = "Dəniz";
+alert("Salam, " + adim + "!");
