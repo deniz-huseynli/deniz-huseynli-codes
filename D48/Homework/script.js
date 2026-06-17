998853b0786a8e5b510db174c06726e9e9d720ba
deniz_huseynli <denizhuseynli2011@gmail.com>
2026-06-17 21:51:32 +0300

    JS Task

---
+++ D48/Homework/script.js
@@ -0,0 +1,21 @@
+let favourite = "Honor of Kings";
+console.log("Mənim ən sevdiyim oyun " + favourite + " oyunudur.");
+
+// const secretCode = 1234;
+// secretCode = 5555;     -Uncaught TypeError: Assignment to constant variable. at script.js:5:12
+
+var car = "BMW";
+document.write("Sərgidə olan birinci maşın: " + car + "<br>");
+car = "Mercedes";
+document.write("Sərgidə olan ikinci maşın: " + car);
+
+let planet = "Mars";
+let spaceship = "Yıldırım";
+alert(spaceship + " gəmisi " + planet + " planetinə eniş etdi!");
+
+// let point = 10;
+// let point = 20;  
+
+var point = 10;
+var point = 20;
+console.log(point);  // var dəyişən növü "səhvə" icazə verir
