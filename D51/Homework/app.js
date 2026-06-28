5121717dc76992cb5aaf927dd137cd3433ab268e
deniz_huseynli <denizhuseynli2011@gmail.com>
2026-06-28 13:44:21 +0300

    JS operatorlar Homework

---
+++ D51/Homework/app.js
@@ -0,0 +1,58 @@
+let x=4;
+let y=3;
+let z=8;
+let a=2;
+let b=5;
+
+++x;
+x+=3;
+--y;
+y*=2;
+++b;
+b+=4;
+b*=2;
+b/=4;
+b**=2;
+--b;
+b%=7;
+
+if ((b>x && (z>(a % 2))) || ((z/y)>1)){
+    console.log("True");
+} else{
+    console.log("False");
+}
+
+
+
+let c=2;
+let d=6;
+let e=5;
+let f=3;
+let g=10;
+
+c+=5;
+--c;
+d/=2;
+d+=7;
+e%=3;
+++e;
+e*=4;
+f**=2;
+f-=4;
+--g;
+g**=2;
+g+=4;
+g%=9;
+
+if ((g>c && f>g) &&(e%3>0)){
+    console.log("True");
+} else{
+    console.log("False");
+}
+
+
+
+
+
+
+
