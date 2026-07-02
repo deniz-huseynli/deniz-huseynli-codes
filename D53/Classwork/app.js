871e7554d187f6aabe49abfd012bf2681d99a415
deniz_huseynli <denizhuseynli2011@gmail.com>
2026-07-02 17:43:33 +0300

    JS Switch Operatoru Classwork

---
+++ D53/Classwork/app.js
@@ -0,0 +1,97 @@
+//Task 1
+let ay = +prompt("Ayın rəqəmini daxil edin: ");
+
+switch (ay){
+    case 1:
+        console.log("Yanvar");
+        break;
+    case 2:
+        console.log("Fevral");
+        break;
+    case 3:
+        console.log("Mart");
+        break;      
+    case 4:
+        console.log("Aprel");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("İyun");
+        break;
+    case 7:
+        console.log("İyul");
+        break;
+    case 8:
+        console.log("Avqust");
+        break;
+    case 9:
+        console.log("Sentyabr");
+        break;
+    case 10:
+        console.log("Oktyabr");
+        break;
+    case 11:
+        console.log("Noyabr");
+        break;
+    case 12:
+        console.log("Dekabr");
+        break;
+
+    default:
+        console.log("Bu ədəd ilə ay yoxdur");
+        break;
+}
+
+
+//Task 2
+let reng = +prompt("Svetoforun rəngini daxil edin:");
+
+switch (reng){
+    case 1:
+        console.log("Qırmızı");
+        break;
+    case 2:
+        console.log("Sarı");
+        break;
+    case 3:
+        console.log("Yaşıl");
+        break;
+    default:
+        console.log("Bu ədəd ilə svetofor rəngi yoxdur");
+        break;
+}
+
+//Task 3
+let planet = +prompt("Planetin nömrəsini daxil edin: ");
+
+switch (planet){
+    case 1:
+        console.log("Merkuri");
+        break;
+    case 2:
+        console.log("Venera");
+        break;
+    case 3:
+        console.log("Yer");
+        break;
+    case 4:
+        console.log("Mars");
+        break;
+    case 5:
+        console.log("Yupiter");
+        break;
+    case 6:
+        console.log("Saturn");
+        break;
+    case 7:
+        console.log("Uran");
+        break;
+    case 8:
+        console.log("Neptun");
+        break;
+    default:
+        console.log("Bu ədəd ilə planet yoxdur");
+        break;
+}
