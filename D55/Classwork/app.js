9c20138a9dcd7c668164ce82450d0df0c24b0e41
deniz_huseynli <denizhuseynli2011@gmail.com>
2026-07-08 14:48:53 +0300

    JS Anonim Funksiyalar Task

---
+++ D55/Classwork/app.js
@@ -0,0 +1,11 @@
+let metn1 = " salam, proqramçı dostlar! ";
+let metn2 = "nicat";
+
+let trimMetn1 = metn1.trim(); 
+let trimUpperMetn = trimMetn1.toUpperCase();
+console.log(trimUpperMetn);
+
+let ilkHerf = metn2.charAt(0);
+let boyukİlkHerf = ilkHerf.toUpperCase();
+let netice = metn2.slice(1);
+console.log(boyukİlkHerf + netice);
---
+++ D55/Homework/app.js
@@ -0,0 +1,41 @@
+//Task 1
+let task1 = "programist";
+console.log(task1.toUpperCase()); 
+
+//Task 2
+let task2 = "CODING";
+console.log(task2.toLowerCase()); 
+
+//Task 3
+let task3 = "Mən proqramlaşdırmanı sevirəm";
+console.log(task3.indexOf("sev")); 
+
+//Task 4
+let task4 = "JavaScript çox maraqlıdır";
+console.log(task4.includes("maraqlıdır")); 
+
+//Task 5
+let task5 = "Salam, dostum!";
+console.log(task5.replace("dostum", "dünya")); 
+
+//Task 6
+let task6 = "   Kod yazmaq əyləncəlidir   ";
+let netice6 = task6.trim();
+console.log(netice6); 
+
+//Task 7
+let task7 = "frontend";
+console.log(task7.slice(0, 5)); 
+
+//Task 8
+let task8 = "HTML, CSS, JavaScript";
+console.log(task8.split(", ")); 
+
+//Task 9
+let task9 = "kitabxana";
+console.log(task9.slice(-3));
+
+//Task 10
+let task10 = "  Bakı gözəl şəhərdir  ";
+let netice10 = task10.trim().toUpperCase().split(" ");
+console.log(netice10); 
