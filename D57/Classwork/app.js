9ecc4b154529bdc17e29acd81f449a40756ce1a1
deniz_huseynli <denizhuseynli2011@gmail.com>
2026-07-15 15:56:01 +0300

    JS Math Task

---
+++ D57/Classwork/app.js
@@ -0,0 +1,16 @@
+//Task 1
+let eliPoints = 120;
+let leylaPoints = 155;
+console.log(Math.abs(eliPoints - leylaPoints));
+
+//Task 2
+let qiymet = 4.6;
+let yuxari = Math.ceil(qiymet);
+console.log("Ceil: " +yuxari);
+
+let asagi = Math.floor(qiymet);
+console.log("Floor: " +asagi);
+
+let riyazi = Math.round(qiymet);
+console.log("Round: " +riyazi);
+
---
+++ D57/Homework/app.js
@@ -0,0 +1,7 @@
+//Task 1
+let suretler =[12, 15, 9, 14, 11, 20];
+console.log("Ən kiçik:", Math.min(...suretler));
+console.log("Ən böyük:", Math.max(...suretler));
+
+//Task 2
+console.log(Math.round(Math.random()*5)+1);
